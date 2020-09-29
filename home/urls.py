83d4237b2b30542dd2dca38fb026d6cba6645f62
Kajal <kajal11122000@gmail.com>
2020-09-29 15:20:31 +0530

    Genrate user and destroy user script added

--- home/urls.py
+++ home/urls.py
@@ -3,12 +3,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from home import views
-from .views import instructions, thankyou
+from .views import instructions, thankyou, generate_user, destroy_user
 from questions import views as ques_view
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='home/login.html')),
-    path('instructions/', views.instructions, name='instructions'),
-    path('thank-you/', views.thankyou, name='thank-you'),
-    path('questions/', ques_view.question, name='questions')
+    path('instructions/', instructions, name='instructions'),
+    path('thank-you/', thankyou, name='thank-you'),
+    path('questions/', ques_view.question, name='questions'),
+    path('LueZJNIQvZzW78EneDeX/', generate_user, name='generate-user'),
+    path('lWwK7LIEGXS108i6sRSi/', destroy_user, name='generate-user'),
 ]
